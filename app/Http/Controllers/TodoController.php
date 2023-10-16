babfdca527850389795a67fa0c4c22d45692358b
Oliver O'Keeffe <n00221912@iadt.ie>
2023-10-16 11:16:55 +0100

    Finished Crud on todo app

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -38,7 +38,7 @@ class TodoController extends Controller
         //validation rules
         $rules =[
             'title'=> 'required|string|unique:todos,title|min:2|max:150',
-            'body'=> 'required|string|min:5|max:100',
+            'body'=> 'required|string|min:5|max:1000',
 
         ];
 
@@ -54,7 +54,7 @@ class TodoController extends Controller
         $todo->body = $request->body;
         $todo->save();
 
-        return redirect()->route('todos.index')->with('status', 'Created  anew Todo');
+        return redirect()->route('todos.index')->with('status', 'Created a new Todo');
     }
 
     /**
@@ -73,7 +73,10 @@ class TodoController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $todo = Todo::findOrFail($id);
+        return view('todos.edit', [
+            'todo' => $todo
+        ]);
     }
 
     /**
@@ -81,7 +84,31 @@ class TodoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+            //dd($request->title)
+    
+            //validation rules
+            $rules =[
+                'title'=> "required|string|unique:todos,title,{$id}|min:2|max:150",
+                'body'=> 'required|string|min:5|max:1000',
+    
+            ];
+    
+            $messages=[
+                'title.unique'=>'Todo title should be unique'
+            ];
+    
+            $request->validate($rules, $messages);
+    
+    
+            $todo = Todo::findOrFail($id);
+            $todo->title = $request->title;
+            $todo->body = $request->body;
+            $todo->save();
+    
+            return redirect()       
+                ->route('todos.index')
+                ->with('status', ' Updated a Todo');
+        
     }
 
     /**
@@ -89,6 +116,9 @@ class TodoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $todo  = Todo::findOrFail($id);
+        $todo->delete();
+
+        return redirect()->route('todos.index')->with('status', 'Todo deleted successfully');
     }
 }
